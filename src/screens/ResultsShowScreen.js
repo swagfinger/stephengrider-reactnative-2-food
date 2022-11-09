4707ab92be1dfed037f202f7f5095e29c2b1dd89
Swag Finger <swagfinger@gmail.com>
2022-11-09 17:21:32 +0800

    COMPLETED Yelp restaurant search

--- src/screens/ResultsShowScreen.js
+++ src/screens/ResultsShowScreen.js
@@ -1,18 +1,51 @@
-import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import React, { useState, useEffect } from 'react';
+import { View, Text, StyleSheet, FlatList, Image } from 'react-native';
+import yelp from '../api/yelp';
+import axios from 'axios';
 
 const ResultsShowScreen = ({ navigation }) => {
   const id = navigation.getParam('id');
 
-  console.log('id: ', id);
+  const [result, setResult] = useState(null);
+
+  console.log('result: ', result);
+
+  const getResult = async (id) => {
+    const response = await yelp.get(`/${id}`);
+    setResult(response.data);
+  };
+
+  //call once only...
+  useEffect(() => {
+    getResult(id);
+  }, []);
+
+  if (!result) {
+    return null;
+  }
 
   return (
     <View>
-      <Text>ResultsShow</Text>
+      <Text>{result.name}</Text>
+      <FlatList
+        data={result.photos}
+        keyExtractor={(photo) => photo}
+        renderItem={({ item }) => {
+          return (
+            <Image style={styles.image} source={item ? { uri: item } : null} />
+          );
+        }}
+      />
     </View>
   );
 };
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  image: {
+    width: '100%',
+    maxWidth: '100%',
+    height: 200
+  }
+});
 
 export default ResultsShowScreen;
