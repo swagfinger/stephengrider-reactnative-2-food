7c4aef991009642bfd45676c496996986ce720f8
Swag Finger <swagfinger@gmail.com>
2022-11-09 17:01:29 +0800

    Communicating between screens - passing id down to another navigation screen

--- src/screens/ResultsShowScreen.js
+++ src/screens/ResultsShowScreen.js
@@ -1,7 +1,11 @@
 import React from 'react';
 import { View, Text, StyleSheet } from 'react-native';
 
-const ResultsShowScreen = (props) => {
+const ResultsShowScreen = ({ navigation }) => {
+  const id = navigation.getParam('id');
+
+  console.log('id: ', id);
+
   return (
     <View>
       <Text>ResultsShow</Text>
